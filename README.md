d9409de5e2ff91d5052e5b17a0725f30a0b52ea0
ghosteon <vladik.nichik@gmail.com>
2018-07-19 12:44:44 +0300

    v3.0

--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 The terminal based version of the [Blackjack](https://en.wikipedia.org/wiki/Blackjack) casino game in Python.
 
 ![game screenshot](screenshot.png)
-
-
-### Bugs
-- issuing the same cards. That is, cards are copied from the finished linkedlist, rather than extracted from it
-- poorly worked logic of the |A|ce. When you re-take the card, the |A|ce does not change the value already recorded in the player's array
--- blackjack.py
+++ blackjack.py
@@ -16,11 +16,6 @@ all_cards = {
     "|K|♣": 10, "|K|♦": 10, "|K|♥": 10, "|K|♠": 10,
     "|A|♣": 1, "|A|♦": 1, "|A|♥": 1, "|A|♠": 1
 }
-         
-
-cards = []
-for card in all_cards:
-  cards.append(card)
 
 
 # Main function, launches all game processes
@@ -29,6 +24,12 @@ def run_game():
     player = []
     dealer = []
     
+    copy_cards = all_cards.copy()
+    
+    cards = []
+    for card in copy_cards:
+        cards.append(card)
+        
 
     # Calculates the amount of points for the player
     def calc_score(name):
@@ -51,19 +52,15 @@ def run_game():
     def start_player():
         
         card1 = random.choice(cards)
+        card1_var = copy_cards.get(card1)
+        del copy_cards[card1]
+        cards.remove(card1)
+        
         card2 = random.choice(cards)
-    
-        card1_var = all_cards.get(card1)
-        card2_var = all_cards.get(card2)
-
-        if int(calc_score(player)) < 12:
-            all_cards["|A|♣"] = 10
-            all_cards["|A|♦"] = 10
-            all_cards["|A|♥"] = 10
-            all_cards["|A|♠"] = 10
-        for card in all_cards:
-            cards.append(card)
-            
+        card2_var = copy_cards.get(card2)
+        del copy_cards[card2]
+        cards.remove(card2)
+        
         player.append(card1_var)
         player.append(card2_var)
         
@@ -79,10 +76,14 @@ def run_game():
     # Settings for the dealer
     def start_dealer():
         card3 = random.choice(cards)
+        card3_var = copy_cards.get(card3)
+        del copy_cards[card3]
+        cards.remove(card3)
+        
         card4 = random.choice(cards)
-    
-        card3_var = all_cards.get(card3)
-        card4_var = all_cards.get(card4)
+        card4_var = copy_cards.get(card4)
+        del copy_cards[card4]
+        cards.remove(card4)
     
         dealer.append(card3_var)
         dealer.append(card4_var)
@@ -99,66 +100,83 @@ def run_game():
     # Artificial intelligence of the dealer
     def give_card_dealer():
         
-        if int(calc_score(dealer)) < 12:
-            all_cards["|A|♣"] = 10
-            all_cards["|A|♦"] = 10
-            all_cards["|A|♥"] = 10
-            all_cards["|A|♠"] = 10
-            for card in all_cards:
-                cards.append(card)
+        if calc_score(dealer) < 11:
+            copy_cards["|A|♣"] = 11
+            copy_cards["|A|♦"] = 11
+            copy_cards["|A|♥"] = 11
+            copy_cards["|A|♠"] = 11
+            
             card_hit_dealer = random.choice(cards)
-            card_hit_dealer_var = all_cards.get(card_hit_dealer)
+            card_hit_dealer_var = copy_cards.get(card_hit_dealer)
+            del copy_cards[card_hit_dealer]
+            cards.remove(card_hit_dealer)
+            
             dealer.append(card_hit_dealer_var)
             
-        elif int(calc_score(dealer)) < 17:
+        elif calc_score(dealer) < 17:
             probability = [0, 1, 2]
             percent33 = random.choice(probability)
-            all_cards["|A|♣"] = 1
-            all_cards["|A|♦"] = 1
-            all_cards["|A|♥"] = 1
-            all_cards["|A|♠"] = 1
-            for card in all_cards:
-                cards.append(card)
+            
+            copy_cards["|A|♣"] = 1
+            copy_cards["|A|♦"] = 1
+            copy_cards["|A|♥"] = 1
+            copy_cards["|A|♠"] = 1
+                
             if percent33 == 1:
                 card_hit_dealer = random.choice(cards)
-                card_hit_dealer_var = all_cards.get(card_hit_dealer)
+                card_hit_dealer_var = copy_cards.get(card_hit_dealer)
+                del copy_cards[card_hit_dealer]
+                cards.remove(card_hit_dealer)
+                
                 dealer.append(card_hit_dealer_var)
                 
            
     # Request for issuing an additional card
     def give_card():
-        hit = input('\n\t\tHit? (y/n)\n')
-        if int(calc_score(dealer)) > 21:
+        if calc_score(dealer) > 21:
             print('\t--->YOU WIN<---')
             reset_game()
             
+        hit = input('\n\t\tHit? (y/n)\n')
+            
         if hit == 'y':
-            if int(calc_score(player)) >= 12:
-                all_cards["|A|♣"] = 1
-                all_cards["|A|♦"] = 1
-                all_cards["|A|♥"] = 1
-                all_cards["|A|♠"] = 1
-            for card in all_cards:
-                cards.append(card)
+            if calc_score(player) <= 11:
+                copy_cards["|A|♣"] = 11
+                copy_cards["|A|♦"] = 11
+                copy_cards["|A|♥"] = 11
+                copy_cards["|A|♠"] = 11
+            else:
+                copy_cards["|A|♣"] = 1
+                copy_cards["|A|♦"] = 1
+                copy_cards["|A|♥"] = 1
+                copy_cards["|A|♠"] = 1
+                
             card_hit = random.choice(cards)
-            card_hit_rev = all_cards.get(card_hit)
+            card_hit_rev = copy_cards.get(card_hit)
+            del copy_cards[card_hit]
+            cards.remove(card_hit)
+            
             player.append(card_hit_rev)
+            
             print('\t   Hit: ' + card_hit)
             print('\t   score: ' + str(calc_score(player)))
-            if int(calc_score(player)) > 21:
+            
+            if calc_score(player) > 21:
                 print('\t--->YOU LOSE<---')
                 reset_game()
                 
             give_card()
 
         elif hit == 'n':
-            if int(calc_score(player)) > int(calc_score(dealer)):
+            if calc_score(player) > calc_score(dealer):
                 print('\t--->YOU WIN!<---')
                 reset_game()
-            elif int(calc_score(player)) < int(calc_score(dealer)):
+                
+            elif calc_score(player) < calc_score(dealer):
                 print('\t   Dealer score: ' + str(calc_score(dealer)) + '\n\t--->YOU LOSE<---')
                 reset_game()
-            elif int(calc_score(player)) == int(calc_score(dealer)):
+                
+            elif calc_score(player) == calc_score(dealer):
                 print('\t--->DRAW<---')
                 reset_game()
 
