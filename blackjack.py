f2d8b270daa521e41a7adf9bae63fbaad7356578
ghosteon <vladik.nichik@gmail.com>
2018-07-30 18:29:08 +0300

    v4.2: fix bugs

--- blackjack.py
+++ blackjack.py
@@ -2,8 +2,6 @@ import random
 import os
 
 
-"""Because of the complexity of the implementation,
-   |A| will always be static"""
 all_cards = {
     "|2|♣": 2, "|2|♦": 2, "|2|♥": 2, "|2|♠": 2,
     "|3|♣": 3, "|3|♦": 3, "|3|♥": 3, "|3|♠": 3,
@@ -25,24 +23,23 @@ money = 500
 
 # Main function, launches all game processes
 def run_game():
-    if money <= 0:
-        print('\t   |Balance: $' + str(money))
-        print('\t   |Insufficient funds')
-        exit()
-     
+
     print('\t   |Balance: $' + str(money))
      
      
-    # For int(bet)ter control over int(bet)s
+    # For better control over bets
     def place_bet():
         global bet
-        bet = input('\t   |Your bet: ')
+        bet = input('\t   |Your bet: $')
     place_bet()
+     
      
     while True:
         try:
             int(bet)
         except ValueError:
+            place_bet()
+        if int(bet) <= 0:
             place_bet()
         else:
             break
@@ -57,17 +54,46 @@ def run_game():
     for card in all_cards:
         cards.append(card)
         
-
+        
     # Calculates the amount of points for the player
-    def calc_score(name):
+    def calculate(arr):
+    
+        scoresult = []
+        for cat in arr:
+            scoresult.append(all_cards.get(cat))
+            
         result = 0
-        for element in name:
+        for element in scoresult:
             result += element
+            
+        if result > 21:
+            all_cards["|A|♣"] = 1
+            all_cards["|A|♦"] = 1
+            all_cards["|A|♥"] = 1
+            all_cards["|A|♠"] = 1
+        elif result < 11:
+            all_cards["|A|♣"] = 11
+            all_cards["|A|♦"] = 11
+            all_cards["|A|♥"] = 11
+            all_cards["|A|♠"] = 11
+                
+        scoresult = []
+        for cat in arr:
+            scoresult.append(all_cards.get(cat))
+            
+        result = 0
+        for element in scoresult:
+            result += element
+            
         return result
         
         
     # Proposal to restart the game after its completion
     def reset_game():
+    
+        if money <= 0:
+            exit()
+    
         reset = input('\n\n\t\tPlay again? (y/n)\n')
         if reset == 'y':
             os.system('clear')
@@ -78,49 +104,42 @@ def run_game():
 
     # Settings for the player
     def start_player():
+    
         global money
         
         card1 = random.choice(cards)
-        card1_var = all_cards.get(card1)
+        player.append(card1)
         cards.remove(card1)
         
         card2 = random.choice(cards)
-        card2_var = all_cards.get(card2)
+        player.append(card2)
         cards.remove(card2)
-        
-        player.append(card1_var)
-        player.append(card2_var)
         
         print('\n\t   Your cards: ' + card1 + ' and ' + card2)
         
-        if card1_var == 1 and card2_var == 10 or card1_var == 10 and card2_var == 1:
+        if all_cards.get(card1) == 1 and all_cards.get(card2) == 10 or all_cards.get(card1) == 10 and all_cards.get(card2) == 1:
             print('\n\t   ----------------\n\t   ---BLACKJACK!---\n\t   ----------------\n\t   --->YOU WIN!<---')
             
             money += round(int(bet) * 1.5)
             print('\t   |Balance: $' + str(money))
             
             reset_game()
-                    
-        print('\t   score: ' + str(calc_score(player)))
 
 
     # Settings for the dealer
     def start_dealer():
+    
         global money
         
         card3 = random.choice(cards)
-        card3_var = all_cards.get(card3)
+        dealer.append(card3)
         cards.remove(card3)
         
         card4 = random.choice(cards)
-        card4_var = all_cards.get(card4)
+        dealer.append(card4)
         cards.remove(card4)
-    
-        dealer.append(card3_var)
-        dealer.append(card4_var)
-      
-        
-        if card3_var == 1 and card4_var == 10 or card3_var == 10 and card4_var == 1:
+
+        if all_cards.get(card3) == 1 and all_cards.get(card4) == 10 or all_cards.get(card3) == 10 and all_cards.get(card4) == 1:
             print('\t   Dealer cards: ' + card3 + ' and ' + card4)
             print('\n\t   ----------------\n\t   ---BLACKJACK!---\n\t   ----------------\n\t   --->YOU LOSE<---')
             
@@ -128,49 +147,52 @@ def run_game():
             print('\t   |Balance: $' + str(money))
             
             reset_game()
+            
         print('\t   Dealer cards: ' + card3 + ' and |?|')
             
             
-    # Artificial intelligence of the dealer
+    # AI of the dealer
     def give_card_dealer():
-        while calc_score(dealer) < 17:
+        
+        while calculate(dealer) < 17:
             card_hit_dealer = random.choice(cards)
-            card_hit_dealer_var = all_cards.get(card_hit_dealer)
+            dealer.append(card_hit_dealer)
             cards.remove(card_hit_dealer)
             print('\t   and ' + card_hit_dealer)
             
-            dealer.append(card_hit_dealer_var)
-                
-           
+            
     # Request for issuing an additional card
     def give_card():
-        global money
     
-        if calc_score(dealer) > 21:
-            print('\n\t   Dealer score: ' + str(calc_score(dealer)) + '\n\n\t   --->YOU WIN!<---')
+        global money
+        
+        push = ''
+        for stack in dealer:
+            push += stack + ' '
+    
+        if calculate(dealer) > 21:
+            print('\n\t   Dealer cards: ' + push + '\n\t   Dealer score: ' + str(calculate(dealer)) + '\n\n\t   --->YOU WIN!<---')
             
             money += int(bet)
             print('\t   |Balance: $' + str(money))
             
             reset_game()
-            
-        if calc_score(player) == 21:
+                
+        if calculate(player) == 21:
             hit = 'n'
         else:
             hit = input('\n\t\tHit me? (y/n)\n')
             
         if hit == 'y':
             card_hit = random.choice(cards)
-            card_hit_rev = all_cards.get(card_hit)
+            player.append(card_hit)
             cards.remove(card_hit)
             
-            player.append(card_hit_rev)
+            print('\t   New card: ' + card_hit)
+            print('\t   Your score: ' + str(calculate(player)))
             
-            print('\t   Hit card: ' + card_hit)
-            print('\t   Your score: ' + str(calc_score(player)))
-            
-            if calc_score(player) > 21:
-                print('\t   Dealer score: ' + str(calc_score(dealer)) + '\n\n\t   --->YOU LOSE<---')
+            if calculate(player) > 21:
+                print('\n\t   Dealer cards: ' + push + '\n\t   Dealer score: ' + str(calculate(dealer)) + '\n\n\t   --->YOU LOSE<---')
                 
                 money -= int(bet)
                 print('\t   |Balance: $' + str(money))
@@ -180,24 +202,25 @@ def run_game():
             give_card()
 
         elif hit == 'n':
-            if calc_score(player) > calc_score(dealer):
-                print('\t   Dealer score: ' + str(calc_score(dealer)) + '\n\n\t   --->YOU WIN!<---')
+            
+            if calculate(player) > calculate(dealer):
+                print('\n\t   Dealer cards: ' + push + '\n\t   Dealer score: ' + str(calculate(dealer)) + '\n\n\t   --->YOU WIN!<---')
                 
                 money += int(bet)
                 print('\t   |Balance: $' + str(money))
                 
                 reset_game()
                 
-            elif calc_score(player) < calc_score(dealer):
-                print('\t   Dealer score: ' + str(calc_score(dealer)) + '\n\n\t   --->YOU LOSE<---')
+            elif calculate(player) < calculate(dealer):
+                print('\n\t   Dealer cards: ' + push + '\n\t   Dealer score: ' + str(calculate(dealer)) + '\n\n\t   --->YOU LOSE<---')
                 
                 money -= int(bet)
                 print('\t   |Balance: $' + str(money))
                 
                 reset_game()
                 
-            elif calc_score(player) == calc_score(dealer):
-                print('\t   Dealer score: ' + str(calc_score(dealer)) + '\n\n\t   --->PUSH<---')
+            elif calculate(player) == calculate(player):
+                print('\n\t   Dealer cards: ' + push + '\n\t   Dealer score: ' + str(calculate(dealer)) + '\n\n\t   ---->PUSH<----')
                 print('\t   |Balance: $' + str(money))
                 
                 reset_game()
